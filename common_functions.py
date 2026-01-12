342e4afe940d32536d0eb9e2273da95ce4429cd4
sanjana260 <sanjanamohan260@gmail.com>
2026-01-12 17:50:37 +0530

    CIFAR project started

---
+++ common_functions.py
@@ -0,0 +1,26 @@
+import torch
+import numpy as np
+def loss_batch(model, loss_func, xb, yb, opt=None):
+    loss = loss_func(model(xb), yb)
+
+    if opt is not None:
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+
+    return loss.item(), len(xb)
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+    for epoch in range(epochs):
+        model.train()
+        for xb, yb in train_dl:
+            loss_batch(model, loss_func, xb, yb, opt)
+
+        model.eval()
+        with torch.no_grad():
+            losses, nums = zip(
+                *[loss_batch(model, loss_func, xb, yb) for xb, yb in valid_dl]
+            )
+        val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums) # Average batch wise validation loss
+
+        print(epoch, val_loss)
